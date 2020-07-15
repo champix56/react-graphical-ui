e9ab0393203df2e45d549b4f7c6606990b2b80a9
alex <desorbaix@free.fr>
2020-07-15 04:59:14 +0200

    made build version of example as doc

---
+++ docs/precache-manifest.0bee28735784f70c04c0ada8040270b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e82e705a35a4ce536ac149e0660c148b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "95828815b83f0348f008",
+    "url": "./static/css/main.d53804bc.chunk.css"
+  },
+  {
+    "revision": "4514b320a3b4c7371de9",
+    "url": "./static/js/2.d9987caf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d9987caf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95828815b83f0348f008",
+    "url": "./static/js/main.0c682ed2.chunk.js"
+  },
+  {
+    "revision": "7c23011b7905718c7918",
+    "url": "./static/js/runtime-main.37101f07.js"
+  }
+]);
