4efd912be203f1cc426efa5922149e1045010ad8
alex <desorbaix@free.fr>
2020-07-15 05:45:18 +0200

    update doc

--- docs/precache-manifest.2c270bb01100dccf987ce35c50525c76.js
+++ docs/precache-manifest.2c270bb01100dccf987ce35c50525c76.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "14a36fc315840664bbb6d2bd495ad57b",
+    "revision": "0ea0dc9dd0f8ab78e2cd0a72496cbff0",
     "url": "./index.html"
   },
   {
-    "revision": "414e0260d9f5ecc29391",
-    "url": "./static/css/main.338724da.chunk.css"
+    "revision": "142ffbaee704af5be2a5",
+    "url": "./static/css/main.b2e28431.chunk.css"
   },
   {
     "revision": "4514b320a3b4c7371de9",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.d9987caf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "414e0260d9f5ecc29391",
-    "url": "./static/js/main.4f827196.chunk.js"
+    "revision": "142ffbaee704af5be2a5",
+    "url": "./static/js/main.7d3815c2.chunk.js"
   },
   {
     "revision": "7c23011b7905718c7918",
